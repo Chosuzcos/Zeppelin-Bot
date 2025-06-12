6c1c8e5f68dbdd2d8a04b914412aa7d699e2a22f
Chosuzcos <91191224+Chosuzcos@users.noreply.github.com>
2025-06-12 14:58:11 +0900

    Update WelcomeMessagePlugin.ts

--- backend/src/plugins/WelcomeMessage/WelcomeMessagePlugin.ts
+++ backend/src/plugins/WelcomeMessage/WelcomeMessagePlugin.ts
@@ -6,9 +6,24 @@ import { WelcomeMessagePluginType, zWelcomeMessageConfig } from "./types";
 
 const defaultOptions: PluginOptions<WelcomeMessagePluginType> = {
   config: {
-    send_dm: false,
+    send_dm: true,
     send_to_channel: null,
-    message: null,
+    message: `
+# 🎉[user][server]へようこそ
+
+サーバーをご利用いただく前に、まずは 📜 [ルール](https://discord.com/channels/1122080011822825475/1122083465895100516) をご確認ください。こちらには、サーバーのご利用にあたって守っていただくべきルールやマナーが記載されています。皆さまが快適に過ごせる環境を整えるため、必ずご一読ください。
+
+また、サーバーに関する最新情報や重要な告知は、📢[告知チャンネル](https://discord.com/channels/1122080011822825475/1124285854890856458) で随時お知らせいたしますので、こちらも定期的にご確認いただくようお願いいたします。
+
+---
+
+### 🛡️ サーバー利用のための手続き
+
+サーバー内の全ての機能を利用するには、🔗[ 認証チャンネル](https://discord.com/channels/1122080011822825475/1122090258897580082) で**Robloxアカウントの紐付け**が必要です。以下のガイドに従って、紐付けを行ってください：
+
+- 💻 [PC版の紐付け方法](https://www.youtube.com/watch?v=SbDltmom1R8)
+- 📱 [モバイル版の紐付け方法](https://www.youtube.com/watch?v=RhC8AIv1Mfk)
+`
   },
 };
 
